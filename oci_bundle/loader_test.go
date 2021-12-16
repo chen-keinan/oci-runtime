479e37f44120cecab214cd21f0df87394f40f0f6
Chen Keinan <Hen.Keinan@gmail.com>
2021-12-16 11:08:00 +0200

    test: add read config file test

--- oci_bundle/loader_test.go
+++ oci_bundle/loader_test.go
@@ -21,3 +21,29 @@ func TestLoadBundle(t *testing.T) {
 		t.Error(fmt.Sprintf("Load bundle want %d got %d", 1, len(fileData)))
 	}
 }
+
+func TestReadFile(t *testing.T) {
+	tests := []struct {
+		name     string
+		relPath  string
+		wantData string
+		wantErr  error
+	}{
+		{name: "readFile good path", relPath: "./fixture/config.json", wantData: "{\n  \"ociVersion\": \"1.0.0\",\n  \"root\": {\n    \"path\": \"path\",\n    \"redaonly\": true\n  }\n}\n", wantErr: nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fp, err := filepath.Abs(tt.relPath)
+			if err != nil {
+				t.Error(err)
+			}
+			gotData, gotErr := ReadFile(fp)
+			if tt.wantData != gotData {
+				t.Errorf("TestReadFile(),not expected value")
+			}
+			if gotErr != nil && tt.wantErr != nil && tt.wantErr.Error() != gotErr.Error() {
+				t.Errorf("TestReadFile(),not expected value")
+			}
+		})
+	}
+}
